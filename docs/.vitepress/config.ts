b0d0486931b0c76dfbbe06514b5e236a88d808af
Edward Chen <hi@edch.top>
2023-07-01 22:16:34 +0800

    init projecT

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,86 @@
+import { getThemeConfig, defineConfig } from '@sugarat/theme/node'
+
+const blogTheme = getThemeConfig({
+  // 文章默认作者
+  author: 'Edward Chen',
+  // 友链
+  // friend: [
+  //   {
+  //     nickname: '粥里有勺糖',
+  //     des: '你的指尖用于改变世界的力量',
+  //     avatar:
+  //       'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
+  //     url: 'https://sugarat.top'
+  //   },
+  //   {
+  //     nickname: 'Vitepress',
+  //     des: 'Vite & Vue Powered Static Site Generator',
+  //     avatar:
+  //       'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTI2NzY1Ng==674995267656',
+  //     url: 'https://vitepress.vuejs.org/'
+  //   }
+  // ],
+  recommend: {
+    showSelf: true
+  },
+  // 开启离线的全文搜索支持（如构建报错可注释下面的配置再次尝试）
+  search: 'pagefind',
+  // popover: {
+  //   title: '公告',
+  //   body: [
+  //     {
+  //       type: 'text',
+  //       content: 'QQ交流群：681489336 🎉🎉'
+  //     },
+  //     { type: 'text', content: '👇公众号👇---👇 微信 👇' },
+  //     {
+  //       type: 'image',
+  //       src: 'https://img.cdn.sugarat.top/mdImg/MTYxNTAxODc2NTIxMA==615018765210'
+  //     },
+  //     {
+  //       type: 'text',
+  //       content: '欢迎大家加群&私信交流'
+  //     },
+  //     {
+  //       type: 'button',
+  //       content: '博客',
+  //       link: 'https://sugarat.top'
+  //     }
+  //   ],
+  //   duration: 0
+  // }
+})
+
+export default defineConfig({
+  extends: blogTheme,
+  lang: 'zh-cn',
+  title: '爱德华',
+  description: '爱德华的博客，基于 vitepress',
+  vite: {
+    optimizeDeps: {
+      include: ['element-plus'],
+      exclude: ['@sugarat/theme']
+    }
+  },
+  lastUpdated: true,
+  themeConfig: {
+    lastUpdatedText: '上次更新于',
+    footer: {
+      // message: '自定义底部内容',
+      copyright:
+        'MIT Licensed | <a target="_blank" href="https://theme.sugarat.top/"> Edward Chen\'s Blog </a>'
+    },
+    logo: '/logo.png',
+    // editLink: {
+    //   pattern:
+    //     'https://github.com/ATQQ/sugar-blog/tree/master/packages/blogpress/:path',
+    //   text: '去 GitHub 上编辑内容'
+    // },
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/edwdch/blog'
+      }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,9 @@
+---
+layout: home
+# 首页部分元素定制
+blog:
+ name: '爱德华的博客'
+#  motto: 简约风的 Vitepress 博客主题
+ inspiring: 还没想好写点啥
+ pageSize: 6
+---
